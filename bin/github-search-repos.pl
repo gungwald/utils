e4e7ba977c3c1e01ccc7665cb78004a2913bb643
William L Chatfield wlc29 <wchatfield@cas.org>
2020-08-06 14:53:45 -0400

    Expanded Github searcher

--- bin/github-search-repos.pl
+++ bin/github-search-repos.pl
@@ -1,37 +1,54 @@
 #!/usr/bin/perl
 
-# Bill Chatfield <bill_chatfield@yahoo.com>
-
-# This program requires Perl's Net::GitHub package which is not installed by
-# default on most systems.
+# Author: Bill Chatfield - https://github.com/gungwald
 
 use strict;
 use warnings FATAL => 'all';
+use open ':std', ':encoding(UTF-8)';    # Makes STDIN, STDOUT, STDERR UTF-8 compatible
+use Data::Dumper;
+use Try::Tiny;
+use Exception::Class ('ModuleInterfaceException');
 
 use constant EXIT_SUCCESS => 0;
 use constant EXIT_FAILURE => 1;
 
-# Import GitHub module or provide instructions for installing it.
+# Import Net::GitHub module or provide instructions for installing it.
+# TODO - The same for Exception::Class - libexception-class-perl
+# TODO - The same for Tiny::Try - libtiny-try-perl???
+# TODO - Convert eval to try/catch
 BEGIN {
-    eval {use Net::GitHub};
+    eval {use Net::GitHub::V3};
     if ($@) {
-        say STDERR "Required Perl module Net::GitHub is not installed: $@";
-        if (-f "/etc/os-release") {
-            my ($os, $osLike) = `. /etc/os-release && echo \$ID && echo \$ID_LIKE`;
-            if ($os =~ /fedora/) {
-                say STDERR "Install it with this command: sudo dnf install perl-Net::GitHub";
-            }
-            elsif ($os =~ /redhat/) {
-                say STDERR "Install it with this command: sudo yum install perl-Net::GitHub";
-            }
-            elsif ($os =~ /debian/ || $osLike =~ /debian/) {
-                say STDERR "Install it with this command: sudo apt install libnet-github-perl";
-            }
+        say STDERR "Please install required modules via one of these options: ";
+        say STDERR '';
+        say STDERR "    Any System:";
+        say STDERR "    (Recommended to get a working version of Net::GitHub)";
+        say STDERR "    sudo cpan -i Try::Tiny Exception::Class Net::GitHub";
+        say STDERR '';
+        say STDERR "    Ubuntu, Debian, Mint:";
+        say STDERR "    (Not recommended due to old and broken libnet-github-perl)";
+        say STDERR "    sudo apt install libtry-tiny-perl libexception-class-perl libnet-github-perl";
+        say STDERR '';
+        say STDERR "    Fedora:";
+        say STDERR "    sudo dnf install perl-Try-Tiny perl-Exception-Class perl-Net-GitHub";
+        exit(EXIT_FAILURE);
+    }
+}
+
+my $count = 0;
+sub printRepos($)
+{
+    my ($repos) = @_;
+    foreach my $repo (@$repos) {
+        $count++;
+        print $count, '. ', $$repo{url}, "\n";
+        if (defined($$repo{description})) {
+            print $$repo{description}, "\n";
         }
         else {
-            say STDERR "Install it with this command: sudo cpan -i Net::GitHub";
+            print "(No description)\n";
         }
-        exit(EXIT_FAILURE);
+        print "\n";
     }
 }
 
@@ -40,15 +57,12 @@ if (@ARGV == 0) {
     exit(EXIT_SUCCESS);
 }
 
-my $github = new Net::GitHub(version => 3, RaiseError => 1);
+my $github = new Net::GitHub::V3(RaiseError => 1);
 my $ghSearch = $github->search;
 my $searchCriteria = join(' ', @ARGV);
-my %searchResults = $ghSearch->repositories({ q => $searchCriteria, sort => 'stars', });
-my $repos = $searchResults{items};
+my $searchRequest = { q => $searchCriteria, sort => 'stars' };
 
-foreach my $repo (@$repos) {
-    print $$repo{name}, "\n";
-    print $$repo{description}, "\n";
-    print "\n";
+printRepos($ghSearch->repositories($searchRequest)->{items});
+while ($ghSearch->has_next_page) {
+    printRepos($ghSearch->next_page->{items});
 }
-
