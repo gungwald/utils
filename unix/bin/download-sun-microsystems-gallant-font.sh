64820db6209693b2b71cbc15f06cd8bb2bce9952
Bill Chatfield <bill_chatfield@yahoo.com>
2020-11-04 16:23:59 -0500

    Added font download

---
+++ unix/bin/download-sun-microsystems-gallant-font.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+whichOutput=`which curl`
+if [ -n "$whichOutput" ]; then
+  curl --create-dirs --output "$HOME"/.fonts/gallant12x22.ttf https://www.mortec.com.wstub.archive.org/download/gallant12x22/gallant12x22.ttf
+else
+  echo $0: no curl to do the download with
+fi
