88328aecf6bce85fca021212d1e800eb7ae35083
Bill Chatfield <bill_chatfield@yahoo.com>
2020-12-12 08:24:38 -0500

    Fixed jamvm installer

--- linux/bin/install-jamvm.sh
+++ linux/bin/install-jamvm.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-sudo apt install openjdk-8-jre-headless=8u77-b03-3ubuntu3 openjdk-8-jre-jamvm=8u77-b03-3ubuntu3 -V
+
+# If the version numbers are not in sync, the JRE/JDK get stuck at 275
+# and jamvm cannot be installed.
+
+OPENJDK_VERSION=8u77-b03-3ubuntu3
+
+sudo apt install -V \
+	openjdk-8-jre-headless="$OPENJDK_VERSION"	\
+	openjdk-8-jre-jamvm="$OPENJDK_VERSION"		\
+	openjdk-8-jdk-headless="$OPENJDK_VERSION"	\
+	openjdk-8-jdk="$OPENJDK_VERSION"		\
+	openjdk-8-jre="$OPENJDK_VERSION"
