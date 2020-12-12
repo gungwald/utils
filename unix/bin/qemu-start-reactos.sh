11bb84ce8eaf7dc4211a1155f40ff92015acb965
Bill Chatfield <bill_chatfield@yahoo.com>
2020-12-11 23:05:11 -0500

    Stupid stuff

--- unix/bin/qemu-start-reactos.sh
+++ unix/bin/qemu-start-reactos.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-#qemu-system-i386 -L . -m 1024 -cdrom ReactOS-0.4.13.iso -hda reactos-hd.img -boot d -localtime -serial file:reactos.log -soundhw ac97
-qemu-system-i386 -L . -m 256 -hda ~/reactos-hd.qcow2 -localtime -serial file:reactos.log -soundhw ac97
+
+qemu-system-i386 -L . -m 256 -hda ~/reactos-hd.qcow2 -localtime \
+	-serial file:reactos.log -soundhw ac97 \
+	-net user,smb=/home/bill
