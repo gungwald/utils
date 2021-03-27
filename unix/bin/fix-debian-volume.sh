3edd3152b8bcad45bdc16db48224d1d1a2206f11
Bill Chatfield <bill_chatfield@yahoo.com>
2021-03-27 16:11:13 -0400

    Found a new control that needed to be set above 0 volume to produce sound

--- unix/bin/fix-debian-volume.sh
+++ unix/bin/fix-debian-volume.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
-# For some reasons this defaults to 0%. It must be increased to hear
-# sound.
-amixer -c 0 cset numid=1,iface=MIXER,name='Headphone Playback Volume' 80
+
+# For some reason, one or all of these settings default to 0%. They must be
+# increased to hear sound.
+
+# To find out what control is set to 0, run alsamixer and press F6 to select
+# the real sound card. Then go through all the controls, increasing them while
+# having the computer "try" to play sound. When you increase the value of a
+# control and start to hear sound, you have found the setting that Debian
+# screwed up and set to 0. Then run "amixer -c 0 controls" to find the
+# matching definition of the control which can be used with a command line
+# like the below commands.
+
+# The card number always appears to be 0. I guess this actually specifies the
+# physical sound card number. That's suprising with all the silly unnecessary
+# layers in the Linux sound system. It seems like the device file should be
+# the correct thing to specify instead of a number.
+CARD_NUMBER=0
+DESIRED_VOLUME="80%"
+
+# Both of these controls have been the source of the "no sound" problem on 
+# actual Debian installs.
+HEADPHONE_VOLUME_CTRL="numid=1,iface=MIXER,name='Headphone Playback Volume'"
+PCM_VOLUME_CTRL="numid=3,iface=MIXER,name='PCM Playback Volume'"
+
+for CTRL in "$HEADPHONE_VOLUME_CTRL" "$PCM_VOLUME_CTRL"
+do
+    amixer -c "$CARD_NUMBER" cset "$CTRL" "$DESIRED_VOLUME"
+done
+
