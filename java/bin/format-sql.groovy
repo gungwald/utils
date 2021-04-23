fe4fc19f7f14061c358d3334b051f07be1373ad0
Bill Chatfield <Bill_Chatfield@gap.com>
2021-04-23 16:41:52 -0400

    Fix SQL Formatter

--- java/bin/format-sql.groovy
+++ java/bin/format-sql.groovy
@@ -1,6 +1,18 @@
 #!/usr/bin/env groovy
 @Grab('com.github.vertical-blank:sql-formatter:2.0.0')
-import static com.github.vertical_blank.sqlformatter.SqlFormatter.of as SqlFormatter
+import com.github.vertical_blank.sqlformatter.SqlFormatter
 import static com.github.vertical_blank.sqlformatter.languages.Dialect.PlSql
 
-args.each { String arg -> println(SqlFormatter(PlSql).format(new File(arg).text)) }
+// If there are no arguments, then read input from stdin.
+if (args.length == 0) {
+    println(cleanup(SqlFormatter.of(PlSql).format(System.in.text)))
+} else {
+    args.each { String arg -> println(cleanup(SqlFormatter.of(PlSql).format(new File(arg).text))) }
+}
+
+String cleanup(String sql) {
+    // Fix corrupted variable references
+    // Remove spaces added unnecessarily
+    return sql.replaceAll(/\$ \{ (\w+) \}/, { Object[] captureGroup -> "\${${captureGroup[1]}}"})
+        .replaceAll(/(\w) \.(\w)/, { Object[] captureGroup -> "${captureGroup[1]}.${captureGroup[2]}"})
+}
