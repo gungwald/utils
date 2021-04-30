565ae7f4628470ae1a78be86cf8ac72f1490b1e3
Bill Chatfield <Bill_Chatfield@gap.com>
2021-04-30 16:59:53 -0400

    Updated SQL Formatter

--- java/bin/format-sql.groovy
+++ java/bin/format-sql.groovy
@@ -1,18 +1,22 @@
 #!/usr/bin/env groovy
 @Grab('com.github.vertical-blank:sql-formatter:2.0.0')
 import com.github.vertical_blank.sqlformatter.SqlFormatter
+import com.github.vertical_blank.sqlformatter.core.FormatConfig
+
 import static com.github.vertical_blank.sqlformatter.languages.Dialect.PlSql
+
+FormatConfig format = FormatConfig.builder().linesBetweenQueries(2).uppercase(true).build()
 
 // If there are no arguments, then read input from stdin.
 if (args.length == 0) {
-    println(cleanup(SqlFormatter.of(PlSql).format(System.in.text)))
+    println(cleanup(SqlFormatter.of(PlSql).format(System.in.text, format)))
 } else {
-    args.each { String arg -> println(cleanup(SqlFormatter.of(PlSql).format(new File(arg).text))) }
+    args.each { String arg -> println(cleanup(SqlFormatter.of(PlSql).format(new File(arg).text, format))) }
 }
 
-String cleanup(String sql) {
+static String cleanup(String sql) {
     // Fix corrupted variable references
     // Remove spaces added unnecessarily
-    return sql.replaceAll(/\$ \{ (\w+) \}/, { Object[] captureGroup -> "\${${captureGroup[1]}}"})
-        .replaceAll(/(\w) \.(\w)/, { Object[] captureGroup -> "${captureGroup[1]}.${captureGroup[2]}"})
+    return sql.replaceAll(/\$ \{ (\w+) \}/, { Object[] captureGroup -> "\${${captureGroup[1]}}" })
+            .replaceAll(/(\w) \.(\w)/, { Object[] captureGroup -> "${captureGroup[1]}.${captureGroup[2]}" })
 }
