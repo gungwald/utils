c47f5707059615c30b2e129d71ea27e04675166e
Bill Chatfield <Bill_Chatfield@gap.com>
2021-05-06 11:38:13 -0400

    Updated SQL Formatter

--- java/bin/format-sql.groovy
+++ java/bin/format-sql.groovy
@@ -2,16 +2,35 @@
 @Grab('com.github.vertical-blank:sql-formatter:2.0.0')
 import com.github.vertical_blank.sqlformatter.SqlFormatter
 import com.github.vertical_blank.sqlformatter.core.FormatConfig
+import groovy.transform.Field
 
 import static com.github.vertical_blank.sqlformatter.languages.Dialect.PlSql
 
-FormatConfig format = FormatConfig.builder().linesBetweenQueries(2).uppercase(true).build()
+@Field
+FormatConfig formatConfig = FormatConfig.builder().linesBetweenQueries(2).uppercase(true).build()
+@Field
+boolean modifyFilesInPlace = false
 
 // If there are no arguments, then read input from stdin.
 if (args.length == 0) {
-    println(cleanup(SqlFormatter.of(PlSql).format(System.in.text, format)))
+    println(format(System.in.text))
 } else {
-    args.each { String arg -> println(cleanup(SqlFormatter.of(PlSql).format(new File(arg).text, format))) }
+    args.each { String arg ->
+        if (arg == "--in-place" || arg == "-i") {
+            modifyFilesInPlace = true
+        } else {
+            File sqlFile = new File(arg)
+            if (modifyFilesInPlace) {
+               sqlFile.text = format(sqlFile.text)
+           } else {
+               println(format(sqlFile.text))
+           }
+        }
+    }
+}
+
+String format(String sql) {
+    return cleanup(SqlFormatter.of(PlSql).format(sql as String, formatConfig as FormatConfig))
 }
 
 static String cleanup(String sql) {
