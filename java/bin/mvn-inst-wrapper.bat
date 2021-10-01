e3289255dc1a1b388e25ee99feffa9959ebee79c
Bill Chatfield <bill_chatfield@yahoo.com>
2021-10-01 17:59:42 -0400

    Wrapper install cross-platform

--- java/bin/mvn-inst-wrapper.bat
+++ java/bin/mvn-inst-wrapper.bat
@@ -1,1 +1,6 @@
-@mvn -N io.takari:maven:wrapper
+@echo off
+setlocal
+for %%j in ("%~dp0..\lib\bsh*.jar") do set BSH_JAR=%%j
+set BSH_SCRIPT=%~dp0..\scripts\%~n0.bsh 
+java -classpath "%BSH_JAR%" bsh.Interpreter %BSH_SCRIPT% %*
+endlocal
