05e3dd9a9cc19521d07d0d0a65d90253e6e78e5e
Bill Chatfield <bill_chatfield@yahoo.com>
2022-01-10 00:17:15 -0500

    Fix headless install

--- unix/bin/dropbox-headless-install.sh
+++ unix/bin/dropbox-headless-install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+mkdir -p "$HOME"/bin
+wget "https://www.dropbox.com/download?dl=packages/dropbox.py"
+chmod a+x dropbox.py
 cd "$HOME"
 wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 .dropbox-dist/dropboxd
