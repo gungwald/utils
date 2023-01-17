2d86401e86506a22e52168d600ba831b193dcfa8
Bill Chatfield <bill_chatfield@yahoo.com>
2023-01-17 14:30:14 -0500

    Implement command line args

--- java/src/ToLowerCase.java
+++ java/src/ToLowerCase.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class ToLowerCase {
     public static void main(String[] args) {
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line.toLowerCase());
+            if (args.length == 0) {
+                toLowerCase(new BufferedReader(new InputStreamReader(System.in)));
+            } else {
+                for (int i = 0; i < args.length; i++) {
+                    toLowerCase(new BufferedReader(new InputStreamReader(new FileInputStream(args[i]))));
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    public static void toLowerCase(BufferedReader in) throws IOException {
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line.toLowerCase());
         }
     }
 }
