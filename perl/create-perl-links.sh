75ff5a00c4e95020ae3c3d439a91e9e775349121
Bill Chatfield <bill_chatfield@yahoo.com>
2023-08-23 12:21:49 -0400

    Must use mklink not ln on Windows

--- perl/create-perl-links.sh
+++ perl/create-perl-links.sh
@@ -2,11 +2,30 @@
 
 SCRIPT_EXTENSION=".pl"
 
+toLowerCase()
+{
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+# The value of `uname -s` is not consistent across the various methods
+# of running sh/bash on Windows. But, the $OS variable always exists on
+# Windows.
+isWindows()
+{
+	if [ "$OS" = "Windows_NT" ]
+	then
+		true
+	else
+		false
+	fi
+}
+
 # The parentheses around the body instead of curly braces runs the body
 # in a sub-shell.
 # $1 - The file name for which the absolute directory is to be determined.
 # Output - The absolute directory path of $1
-absdirname() (
+absdirname()
+(
   cd `dirname "$1"`
   pwd
 )
@@ -18,7 +37,13 @@ BIN_DIR="$CURR_DIR"/bin
 
 for SCRIPT in `ls "$SCRIPT_DIR"/*"$SCRIPT_EXTENSION"`
 do
-  SCRIPT_LINK=`basename "$SCRIPT" "$SCRIPT_EXTENSION"`
-  rm -f "$BIN_DIR"/"$SCRIPT_LINK"
-  ln -s "$SCRIPT" "$BIN_DIR"/"$SCRIPT_LINK"
+  SCRIPT_LINK="$BIN_DIR"/`basename "$SCRIPT" "$SCRIPT_EXTENSION"`
+  rm -f "$SCRIPT_LINK"
+  # "ln" doesn't work on mingw
+  if isWindows
+  then
+  	  cmd //c mklink "$SCRIPT_LINK" "$SCRIPT"
+  else
+  	  ln -s "$SCRIPT" "$SCRIPT_LINK"
+  fi
 done
