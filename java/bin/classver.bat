6cdf98453ad0b49c7674664d284a4bf55ed1498f
Bill Chatfield <bill_chatfield@yahoo.com>
2025-04-14 10:27:39 -0400

    Updated classver

--- java/bin/classver.bat
+++ java/bin/classver.bat
@@ -1,30 +1,19 @@
 @echo off
-set binDir=%~dp0
-set myName=%~n0
-call "%binDir%..\libexec\runjar.bat" "%myName%.jar" %*
-goto :EOF
 
-:: This file is a template for running jar files. Just copy the file and
-:: name the copy the same as the jar file you want to run. No changes need
-:: to be made to the code or these comments.
-::
-:: Command line arguments are passed through to the jar file unchanged.
-::
-:: The jar file itself should be in the same directory as this script or
-:: in one of these directories:
-::
-::      ..\lib
-::      %USERPROFILE%\lib
-::      %USERPROFILE%\Documents\lib
-::
-:: The script runjar.bat is required to be in the same directory as this
-:: script.
-::
-:: This way of running jar files has these advantages:
-:: 1) This script can be put into a directory in the PATH and easily
-::    started from the command line.
-:: 2) Duplication of code to intelligently start a Java program is
-::    minimized. It is encapsulated in runjar.bat, of which there only
-::    needs to be one copy.
-:: 3) It allows the runnable jar files to be organized into a separate
-::    directory than this script.
+setlocal
+
+set THIS=%~n0
+set THIS_DIR=%~dp0
+set LIB_DIR=%THIS_DIR%..\lib
+
+rem Find the highest version of the jar.
+for %%j in ("%LIB_DIR%\%THIS%-*.jar") do (
+	set JAR=%%j
+)
+
+if exist "%JAR%" (
+	java -jar "%JAR%" 
+) else (
+	echo No %THIS% jar file found in %LIB_DIR%.
+)
+endlocal
