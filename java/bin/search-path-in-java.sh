4a4e89f43626ed155541921774fd4b5b44836a76
Bill Chatfield <bill_chatfield@yahoo.com>
2025-07-02 08:00:35 -0400

    Rebuilt readme

--- java/bin/search-path-in-java.sh
+++ java/bin/search-path-in-java.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Desc:Searches PATH for a command name containing a given substring
 # shellcheck disable=SC2006
 binDir=`dirname "$0"` && [ "$binDir" = "." ] && binDir=`pwd`
 topDir=`dirname "$binDir"`
