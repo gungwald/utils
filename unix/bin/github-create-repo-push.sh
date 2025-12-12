d57d2a5202b5fc69e43a74fbd9e2feaf1974a5c8
Bill Chatfield <bill_chatfield@yahoo.com>
2025-12-12 10:28:06 -0500

    Improve script name

--- unix/bin/github-create-repo-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Desc:Creates a remote github repo with the contents of a local git repo.
-
-# Run this at the top directory of a local git repo. The remote github name 
-# will be the name of the source directory.
-gh repo create --public --source "$PWD" --push
