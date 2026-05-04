a2d929c3bcf14ab8a6139edde4d487ec55f88ab2
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-04 02:04:32 -0400

    Number of spaces as an argument

--- perl/scripts/tabs2spaces.pl
+++ perl/scripts/tabs2spaces.pl
@@ -1,14 +1,45 @@
 #!/usr/bin/perl -w
 
-# Process all commmand line arguments.
+# tab2spaces.pl -n2 file-name.c
+# -n defines the number of spaces. Defaults to 4.
+
+# TODO - Test -n switch
+# TODO - Allow stdin to stdout pipeline filter
+
+sub mkspc($)
+{
+    my ($spcCnt) = @_;
+    my $spaces = "";
+    for (my $i = 0; $i < $spcCnt; $i++) {
+        $spaces .= " ";
+    }
+    return $spaces;
+}
+ 
+################
+#             ##
+# Main Program##
+################
+
+my $spcCnt = 4;
+my $spaces = mkspc(spcCnt);
+   
+# Process files in place.
 for $file (@ARGV) {
     my @lines;
-    
+
+    if ($file ~= (($sw,$num) = /^(-s|-n)(\d+)/)) {
+        if ($num > 0) {
+            $spcCnt = $num;
+            $spaces = mkspc($spcCnt);
+        }
+    }    
+
     # Read in file
     open(IN, $file) || die "$file: $!\n";
     while (<IN>) {
-        # Substitute 4 spaces for each tab.
-        s/\t/    /g;
+        # Substitute spaces for each tab.
+        s/\t/$spaces/g;
         push(@lines, $_);
     }
     close(IN);
