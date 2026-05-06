d0dba2e081a05531513b6443c64e8ba64b1c1207
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-06 10:39:12 -0400

    Space count as parameter

--- perl/scripts/tabs2spaces.pl
+++ perl/scripts/tabs2spaces.pl
@@ -3,17 +3,43 @@
 # tab2spaces.pl -n2 file-name.c
 # -n defines the number of spaces. Defaults to 4.
 
-# TODO - Test -n switch
-# TODO - Allow stdin to stdout pipeline filter
-
-sub mkspc($)
+sub makeSpaces($)
 {
-    my ($spcCnt) = @_;
+    my ($spaceCount) = @_;
     my $spaces = "";
-    for (my $i = 0; $i < $spcCnt; $i++) {
+    for (my $i = 0; $i < $spaceCount; $i++) {
         $spaces .= " ";
     }
     return $spaces;
+}
+
+sub doStdio($){
+    my ($spaces) = @_;
+    while (<STDIN>) {
+        s/\t/$spaces/g;
+        print;
+    }
+}
+
+sub usage()
+{
+    my $pgm = `basename \"$0\"`;
+    chomp($pgm);
+    print "\n";
+    print "Usage:\n";
+    print "\n";
+    print "$pgm [-n<space-count>] [-O] <file-name>\n";
+    print "\n";
+    print "    -n space-count is number of spaces.\n";
+    print "       It defaults to 4.\n";
+    print "    -O Write output to standard output.\n";
+    print "       This is consistent with dos2unix.\n";
+    print "\n";
+    print "    file-name: files are modified in place\n";
+    print "        unless the -O switch is provided.\n";
+    print "        It can be - to read from stdin.\n";
+    print "\n";
+    exit(0);
 }
  
 ################
@@ -21,34 +47,65 @@ sub mkspc($)
 # Main Program##
 ################
 
-my $spcCnt = 4;
-my $spaces = mkspc(spcCnt);
+my $spaceCount = 4;
+my $spaces = makeSpaces($spaceCount);
+my $fileWasProvided = 0;
+my $writeToStdout = 0;
    
 # Process files in place.
 for $file (@ARGV) {
     my @lines;
+    my $sw;
+    my $num;
 
-    if ($file ~= (($sw,$num) = /^(-s|-n)(\d+)/)) {
+    if (($sw,$num) = ($file =~ /^(-s|-n)(\d+)/)) {
         if ($num > 0) {
-            $spcCnt = $num;
-            $spaces = mkspc($spcCnt);
+            $spaceCount = $num;
+            $spaces = makeSpaces($spaceCount);
         }
-    }    
+    } elsif ($file =~ /--help|-h|-\?|\/\?|\/h|\/help/) {
+        usage();
+    } elsif ($file eq '-O') {
+        $writeToStdout = 1;
+    } elsif ($file eq '-') {
+        $fileWasProvided = 1;
+        doStdio($spaces);
+    } else {
+        $fileWasProvided = 1;
 
-    # Read in file
-    open(IN, $file) || die "$file: $!\n";
-    while (<IN>) {
-        # Substitute spaces for each tab.
-        s/\t/$spaces/g;
-        push(@lines, $_);
-    }
-    close(IN);
+        if ($writeToStdout) {
+            # Read in file
+            open(IN, $file) || die "$file: $!\n";
+            while (<IN>) {
+                # Substitute spaces for each tab.
+                s/\t/$spaces/g;
+                # Write to stdout.
+                print;
+            }
+            close(IN);
+        } else {
+            # Modify file in place - the default
+            
+            # Read in file
+            open(IN, $file) || die "$file: $!\n";
+            while (<IN>) {
+                # Substitute spaces for each tab.
+                s/\t/$spaces/g;
+                push(@lines, $_);
+            }
+            close(IN);
 
-    # Write out file with spaces instead of tabs.
-    open(F, "> $file") || die "$file: $!\n";
-    for (@lines) {
-        print F $_;
+
+            # Write out file with spaces instead of tabs.
+            open(F, "> $file") || die "$file: $!\n";
+            for (@lines) {
+                print F $_;
+            }
+            close(F);
+        }
     }
-    close(F);
 }
 
+if (! $fileWasProvided) {
+    doStdio($spaces);
+}
