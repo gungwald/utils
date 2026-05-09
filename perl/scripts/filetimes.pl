43a0dea66e4c82acc973bc78a4307cadd6499fdd
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-09 12:38:11 -0400

    Date ISO to US

--- perl/scripts/filetimes.pl
+++ perl/scripts/filetimes.pl
@@ -1,16 +1,18 @@
 #!/usr/bin/env perl
 
+# Desc: Displays create, modify, and access times for files
+
 use POSIX qw(asctime ctime gmtime localtime strftime);
 
-print "ACTION DAY DATE       TIME   TZ  FILE\n";
+print "ACTION DAY DATE            TIME TZ  FILE\n";
 
 for my $file (@ARGV) {
     my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($file);
 
     for my $ref (['Create',$ctime], ['Modify',$mtime], ['Access',$atime]) {
-        my ($label, $time) = @$ref;
-        my $localTime = strftime("%a %Y-%m-%d %l:%M%P %Z", localtime($time));
-        printf("%s %s %s\n", $label, $localTime, $file);
+        my ($action, $time) = @$ref;
+        my $localTime = strftime("%a %b %e, %Y %l:%M%P %Z", localtime($time));
+        printf("%s %s %s\n", $action, $localTime, $file);
     }
 }
 
