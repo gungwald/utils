14514c49e575ddcd217e23d173755e6540269d5b
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-08 23:01:03 -0400

    Improve output format

--- perl/scripts/filetimes.pl
+++ perl/scripts/filetimes.pl
@@ -2,22 +2,16 @@
 
 use POSIX qw(asctime ctime gmtime localtime strftime);
 
+print "ACTION DAY DATE       TIME   TZ  FILE\n";
+
 for my $file (@ARGV) {
-        my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($file);
+    my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($file);
 
-        if (! $ENV{TZ}) {
-                # If the time zone is not set, assume it is Eastern Time.
-                $ENV{TZ} = 'America/New_York';
-        }
-
-        print "\n";
-
-        for my $ref (['Create',$ctime], ['Modify',$mtime], ['Access',$atime]) {
-                my ($label, $time) = @$ref;
-                printf("$label time = %s\n", strftime("%A %e-%b-%Y %l:%M%P UTC (Greenwich Mean Time)", gmtime($time)));
-                printf("$label time = %s\n", strftime("%A %e-%b-%Y %l:%M%P %Z (local time)\n", localtime($time)));
-        }
-
+    for my $ref (['Create',$ctime], ['Modify',$mtime], ['Access',$atime]) {
+        my ($label, $time) = @$ref;
+        my $localTime = strftime("%a %Y-%m-%d %l:%M%P %Z", localtime($time));
+        printf("%s %s %s\n", $label, $localTime, $file);
+    }
 }
 
 
